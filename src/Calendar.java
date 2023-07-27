2cf00859c02d23ab71060545d7f1ecbbf8132e08
Luke-Quarderer <lquarderer13@gmail.com>
2023-07-27 10:37:03 +1000

    Final

---
+++ src/Calendar.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Calendar {
+
+    private List<Month> months;
+    private List<Day> days;
+    private Year currentYear;
+    
+    public Calendar(Year currentYear){
+        this.currentYear = currentYear;
+        this.initialiseMonths();
+        this.initialiseDays();
+    }
+
+    private void initialiseMonths(){
+        months = new ArrayList<Month>();
+        months.add(new Month("January", 31));
+        months.add(new Month("February", 28 ));
+        months.add(new Month("March", 31));
+        months.add(new Month("April", 30));
+        months.add(new Month("May", 31));
+        months.add(new Month("June", 30));
+        months.add(new Month("July", 31));
+        months.add(new Month("August", 31));
+        months.add(new Month("September", 30));
+        months.add(new Month("October", 31));
+        months.add(new Month("November", 30));
+        months.add(new Month("Decemeber", 31));
+    }
+
+    private void initialiseDays(){
+        days = new ArrayList<Day>();
+        for(int i = 0; i < months.size(); i++){
+            for(int n = 0; n < months.get(i).getNumOfDays(); n++){
+                    days.add(new Day(n+1, i+1, currentYear.getYear()));
+            }
+        }
+
+    }
+
+    public List<Month> getMonths() {
+        return months;
+    }
+    public List<Day> getDays() {
+        return days;
+    }
+    
+}
